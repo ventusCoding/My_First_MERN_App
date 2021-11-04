3148a31b6c57daea39721fa75cbbd03d0cdf418b
Anwer Baccar <anwer.baccar.pro@gmail.com>
2021-11-04 16:47:25 +0100

    add users list & users item components

---
+++ src/user/components/UserItem/UserItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from './UserItem.module.css'
+
+const UserItem = (props) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default UserItem
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import UsersList from '../components/UsersList/UsersList'
 
 const Users = () => {
+
+    const USERS = [
+        {
+            id:"u1",
+            image:"https://randomuser.me/api/portraits/men/1.jpg",
+            name:"User 1",
+            placeCount:2
+        },
+        {
+            id:"u2",
+            image:"https://randomuser.me/api/portraits/men/2.jpg",
+            name:"User 1",
+            placeCount:5
+        },
+        {
+            id:"u3",
+            image:"https://randomuser.me/api/portraits/men/3.jpg",
+            name:"User 1",
+            placeCount:6
+        }
+    ];
+
     return (
         <div>
-            <h2>Users Works!</h2>
+            <UsersList items={USERS}/>
         </div>
     )
 }

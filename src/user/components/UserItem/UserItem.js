c3692ebb22c72d4012395813b4d635869a30d5bf
Anwer Baccar <anwer.baccar.pro@gmail.com>
2021-11-04 17:43:09 +0100

    add card component in userItem

--- src/user/components/UserItem/UserItem.js
+++ src/user/components/UserItem/UserItem.js
@@ -2,23 +2,24 @@ import React from 'react';
 import Avatar from '../../../shared/components/UIElements/Avatar/Avatar';
 import './UserItem.css';
 import { Link } from 'react-router-dom';
+import Card from '../../../shared/components/UIElements/Card/Card';
 
 const UserItem = (props) => {
   return (
     <li className="user-item">
-      <div className="user-item__content">
-          <Link to={`/${props.id}/places`}>
-        <div className="user-item__image">
-          <Avatar image={props.image} alt={props.name} />
-        </div>
-        <div className="user-item__info">
-          <h2>{props.name}</h2>
-          <h2>
-            {props.placeCount} {props.placeCount === 1 ? 'Place' : 'Places'}
-          </h2>
-        </div>
+      <Card className="user-item__content">
+        <Link to={`/${props.id}/places`}>
+          <div className="user-item__image">
+            <Avatar image={props.image} alt={props.name} />
+          </div>
+          <div className="user-item__info">
+            <h2>{props.name}</h2>
+            <h2>
+              {props.placeCount} {props.placeCount === 1 ? 'Place' : 'Places'}
+            </h2>
+          </div>
         </Link>
-      </div>
+      </Card>
     </li>
   );
 };

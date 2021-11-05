989d724fe4420276627d0030c0878f1eea31c0d8
Anwer Baccar <anwer.baccar.pro@gmail.com>
2021-11-05 19:44:56 +0100

    placeList

--- src/user/components/PlaceList/PlaceList.js
+++ src/user/components/PlaceList/PlaceList.js
@@ -1,13 +1,37 @@
-import React from 'react'
+import React from 'react';
 
-import './PlaceList.css'
+import './PlaceList.css';
+import Card from '../../../shared/components/UIElements/Card/Card';
+import PlaceItem from '../PlaceItem/PlaceItem';
 
 const PlaceList = (props) => {
+  if (props.items.length === 0) {
     return (
-        <div>
-            
-        </div>
-    )
-}
+      <div className="place-list center">
+        <Card>
+          <h2>No Places found. Maybe Create One ?</h2>
+          <button>Share Place</button>
+        </Card>
+      </div>
+    );
+  }
 
-export default PlaceList
+  return (
+    <ul className="place-list">
+      {props.items.map((place) => (
+        <PlaceItem
+          key={place.id}
+          id={place.id}
+          image={place.imageUrl}
+          title={place.title}
+          description={place.description}
+          address={place.address}
+          creatorId={place.creator}
+          coordinates={place.location}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default PlaceList;

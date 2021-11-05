1f471230b7e72f7efe859ce9761537ebd2608285
Anwer Baccar <anwer.baccar.pro@gmail.com>
2021-11-05 22:34:32 +0100

    add useParams and filter places by userId

--- src/places/pages/UserPlaces/UserPlaces.js
+++ src/places/pages/UserPlaces/UserPlaces.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useParams } from 'react-router';
+
 import PlaceList from '../../../user/components/PlaceList/PlaceList';
 
 const DUMMY_PLACES = [
@@ -31,7 +33,9 @@ const DUMMY_PLACES = [
 ];
 
 const UserPlaces = () => {
-  return <PlaceList items={DUMMY_PLACES} />;
+  const userId = useParams().userId;
+  const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId);
+  return <PlaceList items={loadedPlaces} />;
 };
 
 export default UserPlaces;

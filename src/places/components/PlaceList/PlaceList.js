b9865982cbce98f5b2749ce5ce11e496edd104c3
Anwer Baccar <anwer.baccar.pro@gmail.com>
2021-11-07 16:36:14 +0100

    add Button to placeList

--- src/places/components/PlaceList/PlaceList.js
+++ src/places/components/PlaceList/PlaceList.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './PlaceList.css';
 import Card from '../../../shared/components/UIElements/Card/Card';
 import PlaceItem from '../PlaceItem/PlaceItem';
+import Button from '../../../shared/components/FormElements/Button/Button';
 
 const PlaceList = (props) => {
   if (props.items.length === 0) {
@@ -10,7 +11,7 @@ const PlaceList = (props) => {
       <div className="place-list center">
         <Card>
           <h2>No Places found. Maybe Create One ?</h2>
-          <button>Share Place</button>
+          <Button to="/places/new">Share Place</Button>
         </Card>
       </div>
     );
